0f86a8a01f7613b107388bff68c87d0664eb7faf
Richard Hernandez <richiehernandez@mac.com>
2014-06-13 14:26:22 -0400

    please

--- rci/src/main/java/com/example/rci/rci/LogInActivity.java
+++ rci/src/main/java/com/example/rci/rci/LogInActivity.java
@@ -18,7 +18,7 @@ public class LogInActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.log_in, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
