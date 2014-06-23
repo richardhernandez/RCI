76d464dbcaff08d604aac1ab563cf7ea6c2b7a47
Chunga64 <cjohnston8@gatech.edu>
2014-06-23 11:18:06 -0400

    fb log in

--- rci/src/main/java/com/example/rci/rci/LogInActivity.java
+++ rci/src/main/java/com/example/rci/rci/LogInActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +16,8 @@ import android.widget.Toast;
 
 import com.facebook.UiLifecycleHelper;
 import com.facebook.android.Facebook;
+import com.facebook.*;
+import com.facebook.model.GraphUser;
 
 
 public class LogInActivity extends Activity {
@@ -24,15 +27,42 @@ public class LogInActivity extends Activity {
     private ImageButton facebook;
     private String APP_ID;
     private Facebook fb;
+    //private LogInFragment logInFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_log_in);
         email = (EditText)findViewById(R.id.emailInputText);
         password = (EditText)findViewById(R.id.passwordInputText);
         login = (Button)findViewById(R.id.button1);
         facebook = (ImageButton)findViewById(R.id.imageButton);
+        facebook.setVisibility(View.INVISIBLE);
+
+        // start Facebook Login
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+
+                if(session.isOpened()){
+                    // make request to the /me API
+                    Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                        // callback after Graph API response with user object
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            email.setText(user.getUsername());
+
+                            Toast.makeText(getApplicationContext(), "Click Log in to Proceed", Toast.LENGTH_SHORT).show();
+                        }
+                    }).executeAsync();
+                }
+            }
+        });
     }
 
     //Function to make login work
@@ -54,6 +84,11 @@ public class LogInActivity extends Activity {
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
     //Function to make facebook login work
     public void PullFacebookData(View view) {
         email.setText("gburdell@gatech.edu");
